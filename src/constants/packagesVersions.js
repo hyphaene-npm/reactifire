b95f1d7c98a83306fc46ba5757e129628531e57a
Maximilien Garenne <maximilien.garenne@gmail.com>
2019-10-23 17:00:30 +0200

    [latest-version] add latest version

--- src/constants/packagesVersions.js
+++ src/constants/packagesVersions.js
@@ -1,2 +1,6 @@
-module.exports.REACT_PV = '^16.5.2';
+const latestVersion = require('latest-version');
+
+module.exports.REACT_PV = (async () => {
+	await latestVersion('react');
+})();
 module.exports.PROPTYPES_PV = '^15.6.1';
